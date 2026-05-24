26137dc0a335edb483064f5e0ec781d7b1f5e912
Ementshh <clementheo.benaya@ui.ac.id>
2026-05-24 09:55:18 +0700

    Working di chrome. Gabisa login

--- Game/html/src/main/java/com/rummikub/gwt/GwtLauncher.java
+++ Game/html/src/main/java/com/rummikub/gwt/GwtLauncher.java
@@ -9,14 +9,15 @@ import com.rummikub.RummikubGame;
 public class GwtLauncher extends GwtApplication {
         @Override
         public GwtApplicationConfiguration getConfig () {
-            // Disable physical pixels to avoid texture scaling bugs on some retina web displays
-            GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(false);
-            cfg.padVertical = 0;
-            cfg.padHorizontal = 0;
-            // Explicitly disable WebGL 2.0 to prevent the 'drawElementsInstanced' index buffer crash
-            cfg.useGL30 = false;
-            return cfg;
-        }
+        GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(1280, 720);
+        cfg.padVertical = 0;
+        cfg.padHorizontal = 0;
+        
+        cfg.useGL30 = true; 
+        
+        return cfg;
+    }
+
         @Override
         public ApplicationListener createApplicationListener () {
             return new RummikubGame();
